af07524c7047880fb150fdee38c166707f6198b2
MaRyamFatima1120 <maryamfatimanavtcc@gmail.com>
2024-12-15 13:19:47 +0500

    Upload the Project With Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:fitnest_app/src/routes/route.dart';
 import 'package:fitnest_app/src/themes/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
